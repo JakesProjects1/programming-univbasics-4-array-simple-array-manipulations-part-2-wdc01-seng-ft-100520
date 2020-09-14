e4dc2bf1f3752c107cda96bd9084d8feacf34088
Jacob Kagon <jacob@kagon.net>
2020-09-14 19:23:53 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+@my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
+more_favs = ["sports cars", "flatiron school"]
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+using_concat(@my_favorite_things, more_favs)
+
+ @list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
+   @another_language = "Python"
+ 
+def using_insert(array, element)
+  new_array =[]
+  new_array = 
+  
+end 
+
+using_insert(@list_of_programming_languages, "Python")
